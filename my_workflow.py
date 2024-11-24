b438caa8c291236bea1b536eab5ca145b0d98ba8
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2024-11-24 17:50:21 +0200

    Update my_workflow.py

--- my_workflow.py
+++ my_workflow.py
@@ -9,7 +9,7 @@ def kfpipeline():
     project = mlrun.get_current_project()
     func = project.get_function("my-func")
     func.with_preemption_mode("allow")
-    func.with_node_selection(node_selector={"app.iguazio.com/node-group": "added-wr17wzokx4"})
+    #func.with_node_selection(node_selector={"app.iguazio.com/node-group": "added-wr17wzokx4"})
     func.save()
     # step = func.run().set_retry(policy="Always",num_retries=1)
     # Put the kfp pod on a constant node
