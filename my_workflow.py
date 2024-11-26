b8ad4027c09d2be18074cfc008986972a92fb1bc
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2024-11-26 10:43:46 +0200

    Update my_workflow.py

--- my_workflow.py
+++ my_workflow.py
@@ -7,14 +7,15 @@ from kfp.dsl import ContainerOp
 @dsl.pipeline()
 def kfpipeline():
     project = mlrun.get_current_project()
-    func = project.get_function("my-func")
-    func.with_preemption_mode("constrain")
+    func = project.get_function("my-func", ignore_cache=True)
+    #func.with_preemption_mode("constrain")
     #func.with_node_selection(node_selector={"app.iguazio.com/node-group": "added-wr17wzokx4"})
-    func.save()
+    #func.save()
     # step = func.run().set_retry(policy="Always",num_retries=1)
     # Put the kfp pod on a constant node
     
-    step = mlrun.run_function(name="step1", function="my-func")
+    #step = mlrun.run_function(name="step1", function="my-func")
+    step = func.run()
     # step.node_selector={"app.iguazio.com/node-group": "added-ondemand"}
     
     
