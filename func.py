3e6a876d10b7453f42f55d4ccab8b8781ff1cfe3
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2024-11-27 11:57:15 +0200

    Update func.py

--- func.py
+++ func.py
@@ -1,3 +1,13 @@
-def func(context):
-    print("Hi from inside the function")
+def func_allow(context):
+    print("Hi from inside the func_allow")
+    return 1
+
+
+def func_prevent(context):
+    print("Hi from inside the func_prevent")
+    return 1
+
+
+def func_constraint(context):
+    print("Hi from inside the func_constraint")
     return 1
