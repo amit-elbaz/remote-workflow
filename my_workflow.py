afd51e17a79bb260d4961f3d92fad97b1ede5136
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2024-11-27 12:02:00 +0200

    Update my_workflow.py

--- my_workflow.py
+++ my_workflow.py
@@ -25,17 +25,17 @@ def kfpipeline():
     project = mlrun.get_current_project()
     func_allow = project.get_function("my-func-allow")
     func_prevent = project.get_function("my-func-prevent")
-    func_constraint = project.get_function("my-func-constraint")
+    func_constrain = project.get_function("my-func-constrain")
     
     func_allow.with_preemption_mode("allow")
     func_prevent.with_preemption_mode("prevent")
-    func_constraint.with_preemption_mode("constraint")
+    func_constrain.with_preemption_mode("constrain")
     
     func_allow.save()
     func_prevent.save()
-    func_constraint.save()
+    func_constrain.save()
     
     allow_step = mlrun.run_function(name="allow_func", function="my-func-allow")
     prevent_step = mlrun.run_function(name="prevent_func", function="my-func-prevent")
-    constraint_step = mlrun.run_function(name="constraint_func", function="my-func-constraint")
+    constrain_step = mlrun.run_function(name="constrain_func", function="my-func-constrain")
     
